f49832af4398aed42a663db5babae0d1d3e45021
pedrocruz <JayJMarquez05@gmail.com>
2024-11-07 12:15:34 -0600

    Route is working and it returned back our test

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,1 +1,15 @@
 const Pet = require("../models/pet.js");
+const express = require("express");
+const router = express.Router();
+
+// Writing my controller here
+
+// C.R.U.D
+// Create /-/ Update Delete
+
+router.post(`/`, (req, res) => {
+  res.json({ message: `Create Route` });
+});
+
+// Exporting this router at the bottom
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const dotenv = require("dotenv");
+const dotenv = require(`dotenv`);
 dotenv.config();
-const express = require("express");
+const express = require(`express`);
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require(`mongoose`);
+
+// Import the controller file
+const petRouter = require(`./controllers/pets.js`);
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on(`connected`, () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.json());
 
-// Routes go here
+// Our routes go here
+app.use(`/pets`, petRouter);
 
 app.listen(3000, () => {
-  console.log("The express app is ready!");
+  console.log(`The express app is ready!`);
 });
